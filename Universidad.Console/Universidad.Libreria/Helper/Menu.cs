0529561773fb62fa4e501d413a53a8ee151a8d02
Federico Rampazzo <federamp91@gmail.com>
2021-04-22 12:12:44 -0300

    Validador y Menu

---
+++ Universidad.Console/Universidad.Libreria/Helper/Menu.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Universidad.Libreria.Helper
+{
+    public class Menu
+    {
+
+        List<string> opciones = new List<string>();
+        private string titulo;
+
+        public Menu(string titulo, string[] opciones)
+        {  //Constructor
+            this.titulo = titulo;
+            this.opciones = opciones.ToList();
+        }
+
+        public string print()
+        {
+            string resultado = "";
+            resultado += titulo + "\n";
+            try
+            {
+                if (!isEmpty())
+                {
+                    for (int i = 0; i < opciones.Count; i++)
+                    {
+                        resultado += ("\t" + (i + 1) + ". " + opciones[i] + "\n");
+                    }
+                }
+            }
+            catch (Exception ave)
+            {
+                Console.WriteLine(ave.Message);
+            }
+            return resultado;
+        }
+
+        public int elegir()
+        {
+            try
+            {
+                int opcionElegida = Validador.pedirInt(print());
+                string opcion = opciones[opcionElegida - 1]; //Esto me va a tirar el error 
+                return opcionElegida - 1;
+            }
+            catch (IndexOutOfRangeException iore)
+            {
+                Console.WriteLine("No existe esa opcion");
+            }
+            return -1;
+        }
+
+        public bool isEmpty()
+        {
+            if (this.opciones.Count == 0)
+            {
+                throw new Exception("Menu sin opciones");
+            }
+            return !(this.opciones.Count > 0);
+        }
+    }
+}
---
+++ Universidad.Console/Universidad.Libreria/Helper/Validador.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Universidad.Libreria.Helper
+{
+    public class Validador
+    {
+
+
+        public static string pedirString(string mensaje)
+        {
+            string resultado = "";
+            do
+            {
+                Console.WriteLine(mensaje);
+                resultado = Console.ReadLine();
+                if (resultado == "") Console.WriteLine("Algo, no vacío");
+            } while (resultado == "");
+
+            return resultado;
+        }
+
+        public static int pedirInt(string mensaje)
+        {
+            string entrada = "";
+            int resultado;
+            while (entrada == "")
+            {
+                try
+                {
+                    Console.WriteLine(mensaje);
+                    resultado = int.Parse(Console.ReadLine());
+                    return resultado;
+
+                }
+                catch (FormatException fe)
+                {
+                    Console.WriteLine("Formato incorrecto");
+                    entrada = "";
+                }
+            }
+            return 0;
+
+        }
+    }
+}
